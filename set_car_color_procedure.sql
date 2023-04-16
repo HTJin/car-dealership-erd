a22ce266d5412dceaf34127a8ec98e4bccac38f4
Hyun-Tae Jin <htjmario@gmail.com>
2023-04-16 06:17:00 -0400

    redefined regex, procedure updates based on record

--- set_car_color_procedure.sql
+++ set_car_color_procedure.sql
@@ -4,13 +4,21 @@ ADD COLUMN color VARCHAR(50);
 CREATE OR REPLACE PROCEDURE update_car_color()
 LANGUAGE PLPGSQL AS $$
 BEGIN
-    UPDATE car
-    SET color = (
-        SELECT SUBSTRING(record.description FROM '\sold a (\w+)')
+    WITH color_subquery AS (
+        SELECT DISTINCT ON (record.car_id)
+            record.car_id,
+            SUBSTRING(
+                record.description
+                FROM '(?<=sold a\s|sell a\s)(\w+)(?=\s[A-Za-z0-9\s-]+,)'
+            ) AS car_color
         FROM record
-        WHERE car.car_id = record.car_id
+        ORDER BY record.car_id, record.record_id DESC
     )
-    WHERE color IS NULL;
+    UPDATE car
+    SET color = color_subquery.car_color
+    FROM color_subquery
+    WHERE car.car_id = color_subquery.car_id;
 END;
 $$;
+
 CALL update_car_color();
